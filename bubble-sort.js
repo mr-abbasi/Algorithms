1d9c3df4bbd503357b65227fa67254bac796850b
Mohammad Abbasi <elanteria@gmail.com>
2020-05-20 21:34:08 +0430

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,11 +1,16 @@
 function bubbleSort(array) {
+    let swappHappened = false;
     for (let i = 1; i < array.length; i++) {
         for (let j = 0; j <= array.length - i - 1; j++) {
             if (array[j] > array[j + 1]) {
+                swappHappened = true;
                 const temp = array[j + 1];
                 array[j + 1] = array[j];
                 array[j] = temp;
             }
+        }
+        if (!swappHappened) {
+            return array;
         }
     }
 

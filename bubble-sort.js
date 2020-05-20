26f3e4b5253ce53ff5719992b03029b4d773225c
Mohammad Abbasi <elanteria@gmail.com>
2020-05-20 21:19:09 +0430

    Create bubble-sort.js

---
+++ bubble-sort.js
@@ -0,0 +1,13 @@
+function bubbleSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        for (let j = 0; j <= array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+
+    return array;
+}
